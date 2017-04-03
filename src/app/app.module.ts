8e92fe948cd39001b81524fe8deeaeed46c976fe
Sergey Ostapenko <sergey_ostapenko@epam.com>
2017-04-03 17:16:00 +0200

    Added components for page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesComponent,
+    CourseDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
